ec0dfd53985a8825ac0aa88686d2038a2cb97045
ShwetaBodake <shwetabodake8108@gmail.com>
2022-12-02 08:25:02 +0530

    Fliping_Coin

--- Flip_Coin_simulator.sh
+++ Flip_Coin_simulator.sh
@@ -1,1 +1,10 @@
 echo "-------Flip Coin Simulator----------";
+ FlipCoin=$(($RANDOM%2));
+ if [ $FlipCoin -eq 0 ]
+ then
+ 	echo "Afte coin flip HEAD is present ";
+ 	echo "HEAD is Winner"
+ else 
+ 	echo "After Fliping coin TAIL is Present"
+
+ fi
